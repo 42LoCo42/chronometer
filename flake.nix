8aeed79838ad3f3563e21171fbe85e9630a42f9a
Leon Schumacher <leonsch@protonmail.com>
2024-10-02 10:07:56 +0200

    add package output to flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,28 @@
 {
   outputs = { flake-utils, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in {
+      let pkgs = import nixpkgs { inherit system; }; in rec {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "chronometer";
+          version = "1";
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            nodePackages.typescript
+          ];
+
+          buildPhase = ''
+            tsc --build
+          '';
+
+          installPhase = ''
+            cp -r www $out
+          '';
+        };
+
         devShells.default = pkgs.mkShell {
+          inputsFrom = [ packages.default ];
           packages = with pkgs; [
-            nodePackages.typescript # tsc
             nodePackages.typescript-language-server
             python3 # http.server
           ];
